c76da4230b097dcbc1b3ee767da80eb4c56baa36
smskelley <smskelley@gmail.com>
2013-05-18 21:45:34 -0700

    Add tests for class Imdb and use new Video getters
    Needs more tests

--- VideoTests/LibraryTests.cs
+++ VideoTests/LibraryTests.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VideoManagement;
 
 namespace VideoTests
 {
+    [TestClass]
+    public class ImdbTests
+    {
+        const string title = "Django Unchained";
+        const ushort year = 2012;
+
+        [TestMethod]
+        public void GetJsonFieldParsesCorrectly()
+        {
+            string sample = @"[{""genres"": [""Drama"", ""Western""], ""runtime"": [""141 min"", ""165 min""], ""language"": [""English""], ""title"": ""Django Unchained"", ""poster"": ""http://ia.media-imdb.com/images/M/MV5BNjI4NzkzODM3Nl5BMl5BanBnXkFtZTcwMzk1NTIyOA@@._V1._SY317_CR0,0,214,317_.jpg"", ""imdb_url"": ""http://www.imdb.com/title/tt1853728/"", ""filming_locations"": ""Second Line Stages, New Orleans, Louisiana, USA"", ""imdb_id"": ""tt1853728"", ""directors"": [""Quentin Tarantino""], ""writers"": [""Quentin Tarantino""], ""actors"": [""Leonardo DiCaprio"", ""Jonah Hill"", ""Samuel L. Jackson"", ""Christoph Waltz"", ""Jamie Foxx"", ""Kerry Washington"", ""Zoe Bell"", ""Amber Tamblyn"", ""James Remar"", ""Walton Goggins"", ""Don Johnson"", ""Bruce Dern"", ""Robert Carradine"", ""Franco Nero"", ""James Russo""], ""plot_simple"": ""With the help of his mentor, a slave-turned-bounty hunter sets out to rescue his wife from a brutal Mississippi plantation owner."", ""year"": 2012, ""country"": [""USA""], ""type"": ""M"", ""release_date"": 20121225, ""also_known_as"": [""Django sin cadenas""]}]";
+            
+            Assert.AreEqual("English", Imdb.GetJsonField(sample, "language"));
+            Assert.AreEqual("Drama", Imdb.GetJsonField(sample, "genres"));
+            Assert.AreEqual("Django Unchained", Imdb.GetJsonField(sample, "title"));
+        }
+
+        [TestMethod]
+        public void GetJsonDict()
+        {
+            //Dictionary<string,string> dict = (new Imdb(title, year)).GetJsonFromApi();
+            //Assert.AreEqual("Django Unchained", dict["title"]);
+        }
+    }
     [TestClass]
     public class LibraryTests
     {
@@ -43,7 +67,7 @@ namespace VideoTests
             Videos.Add(name, genre, description, rating);
 
             // asserts
-            Assert.AreEqual((Videos.GetByName(name)).GetName(), name, "Video found does not match in name");
+            Assert.AreEqual((Videos.GetByName(name)).Title, name, "Video found does not match in name");
             Assert.IsTrue(Videos.GetByName("doesn't exist") == null, "GetByName found something that it shouldn't have");
         }
     }
