bbc72dc19b0bba25365b062fca8037728a9b52c4
smskelley <smskelley@gmail.com>
2013-05-20 21:49:38 -0700

    Add remove button, use jagged array to store favorites, add exception handling, and support both TVShows and Movie libraries.

--- VideoManagementWpf/MainWindow.xaml.cs
+++ VideoManagementWpf/MainWindow.xaml.cs
@@ -21,26 +21,78 @@ namespace VideoManagementWpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        Library Movies;
+        private Library Movies, TVShows;
+
+        // PersonalFavorites includes both TV Shows and Movies that I enjoy.
+        // PersonalFavorites[0] is responsible for storing TV Shows, while
+        // PersonalFavorites[1] is responsible for storing Movies.
+        private readonly Video[][] PersonalFavorites = new Video[2][];
+
         public MainWindow()
         {
             InitializeComponent();
             Movies = new Library(TypeOfVideo.Movie);
-            Movies.Add("Django", "Comedy", "A great movie about a lizard", 9.8f);
+            TVShows = new Library(TypeOfVideo.TVShow);
+
+            // TV Shows
+            PersonalFavorites[0] = new Video[] {
+                new Video("Planet Earth", 
+                          "Documentary", 
+                          "A voyage around the world with many remarkable sights", 
+                          2006),
+                new Video("Game of Thrones", 
+                          "Fantasy", 
+                          "Seven noble families fight for control of the mythical land of Westeros.", 
+                          2011)
+            };
+            PersonalFavorites[1] = new Video[] {
+                new Video("Django Unchained",
+                          "Adventure",
+                          "A freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.",
+                          2012)
+            };
+
+            foreach (Video tvShow in PersonalFavorites[0])
+                TVShows.Add(tvShow);
+
+            foreach (Video movie in PersonalFavorites[1])
+                Movies.Add(movie);
+
             UpdateListBox();
         }
 
         private void UpdateListBox()
         {
             LibraryListBox.Items.Clear();
-            foreach (Video video in Movies.Videos)
-                LibraryListBox.Items.Add(video);
+
+            foreach (Video movie in Movies.Videos)
+                LibraryListBox.Items.Add(movie);
+            foreach (Video tvShow in TVShows.Videos)
+                LibraryListBox.Items.Add(tvShow);
         }
 
         private void AddVideoButton_Click(object sender, RoutedEventArgs e)
         {
+            // Make sure both inputs are even present
+            if (NewVideoTextBox.GetLineLength(0) == 0 || NewVideoYearTextBox.GetLineLength(0) == 0)
+                return;
+
+            // Convert the string for year to an unsigned short
             ushort year = Convert.ToUInt16(NewVideoYearTextBox.Text);
-            Movies.AddVideoUsingImdb(NewVideoTextBox.Text, year);
+
+            // Many exceptions could be thrown during the proccess of grabbing information from
+            // imdbapi.org, so we will attempt to catch them here.
+            try
+            {
+                // Attempt to add a new video using imdbapi.org to fill in details
+                Movies.AddVideoUsingImdb(NewVideoTextBox.Text, year);
+            }
+            catch
+            {
+                MessageBox.Show("Error: Could not add " + NewVideoTextBox.Text + " (" + year + ").");
+                return;
+            }
+
             UpdateListBox();
         }
 
@@ -53,10 +105,18 @@ namespace VideoManagementWpf
                 TitleContent.Content = selection.Title;
                 YearContent.Content = selection.Year;
                 GenreContent.Content = selection.Genre;
-                RatingContent.Content = selection.Rating;
                 DescriptionContent.Text = selection.Description;
             }
         }
 
+        private void RemoveSelectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            Video selection = (Video)LibraryListBox.SelectedItem;
+            if (selection != null)
+            {
+                Movies.Remove(selection.Title);
+                UpdateListBox();
+            }
+        }
     }
 }
